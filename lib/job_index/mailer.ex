9932eff73a59328cf81e2fb9166e1f4444461c7e
Brian E. McElaney <brian.e.mcelaney@gmail.com>
2023-05-16 02:03:23 -0400

    Add Credo and fix issues

--- lib/job_index/mailer.ex
+++ lib/job_index/mailer.ex
@@ -1,3 +1,4 @@
 defmodule Jin.Mailer do
+  @moduledoc false
   use Swoosh.Mailer, otp_app: :job_index
 end
--- lib/job_index_web/components/layouts.ex
+++ lib/job_index_web/components/layouts.ex
@@ -1,4 +1,5 @@
 defmodule JinWeb.Layouts do
+  @moduledoc false
   use JinWeb, :html
 
   embed_templates "layouts/*"
